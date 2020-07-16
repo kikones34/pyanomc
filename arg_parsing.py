3e721daa4b525bf722d1f4bf7c260f3658924d89
kikones34 <kikones34@hotmail.es>
2020-07-16 21:12:32 +0200

    Perform general cleanup, add support for multi-track midi files, extract tempo, key and time sig from midi file

--- arg_parsing.py
+++ arg_parsing.py
@@ -1,5 +1,6 @@
 """ Functions related to CLI argument parsing """
 
+
 def parse_args(args, arg_parsers):
     for k, v in args.items():
         if k in arg_parsers:
@@ -8,11 +9,14 @@ def parse_args(args, arg_parsers):
                 exit(1)
             args[k] = parsed_val
 
+
 def int_parser(minv=None, maxv=None):
     return lambda arg, val: num_parser_impl(arg, val, minv=minv, maxv=maxv, cast=int)
 
+
 def float_parser(minv=None, maxv=None):
     return lambda arg, val: num_parser_impl(arg, val, minv=minv, maxv=maxv, cast=float)
+
 
 def num_parser_impl(arg, val, minv=None, maxv=None, cast=None):
     cast_str = {int: "an integer", float: "a number"}
@@ -31,8 +35,10 @@ def num_parser_impl(arg, val, minv=None, maxv=None, cast=None):
             return None
     return cast_val
 
+
 def note_parser():
     return lambda arg, val: note_parser_impl(arg, val)
+
 
 def note_parser_impl(arg, val):
     letter, *rest = val
@@ -41,7 +47,8 @@ def note_parser_impl(arg, val):
         octave = int(''.join(rest[1:]))
     else:
         octave = int(''.join(rest))
-    pitches = {"C": 0, "C#": 1, "Db": 1, "D": 2, "D#": 3, "Eb": 3, "E": 4, "F": 5, "F#": 6, "Gb": 6, "G": 7, "G#": 8, "Ab": 8, "A": 9, "A#": 10, "Bb": 10, "B": 11}
+    pitches = {"C": 0, "C#": 1, "Db": 1, "D": 2, "D#": 3, "Eb": 3, "E": 4, "F": 5, "F#": 6, "Gb": 6, "G": 7, "G#": 8,
+               "Ab": 8, "A": 9, "A#": 10, "Bb": 10, "B": 11}
     pitch = pitches[letter]
-    midi_note = (octave+1)*12 + pitch
+    midi_note = (octave + 1) * 12 + pitch
     return midi_note
--- pyanomc.py
+++ pyanomc.py
@@ -36,19 +36,20 @@ Nomenclature:
 
 """
 
+import mido
+from docopt import docopt
+
 from arg_parsing import *
 
 arg_parsers = {
     "--hands": int_parser(minv=1),
     "--fingers": int_parser(minv=1),
-    "--span": float_parser(), 
+    "--span": float_parser(),
     "--max-notes": int_parser(),
     "--lowest-key": note_parser(),
     "--highest-key": note_parser()
 }
 
-import mido
-from docopt import docopt
 
 # utility functions
 def m2h_note(note):
@@ -58,29 +59,60 @@ def m2h_note(note):
     pitch = pitches[note % 12]
     return pitch + str(octave)
 
+
 def white_key_index(note):
     key_index = [0, 0.5, 1, 1.5, 2, 3, 3.5, 4, 4.5, 5, 5.5, 6]
     octave = note // 12
     key = note % 12
-    index = octave*7 + key_index[key]
+    index = octave * 7 + key_index[key]
     return index
+
 
 def key_distance(n1, n2):
     return white_key_index(n2) - white_key_index(n1)
 
-class Config():
-    """ This class provides a cleaner API than just using the args dictionary directly.
-        It transforms the CLI parameters into class attributes (leading dashes are removed and
-        dashes inside the name are replaced with an underscore).
-        It also sets up some functions to perform actions which depend on the configuration.
+
+def midi_time_to_seconds(time, ppqn, tempo):
+    return mido.tick2second(time, ppqn, tempo)
+
+
+def midi_time_to_measure(time, ppqn, timesig):
+    beat = time / ppqn
+    beats_per_measure = 4 * (timesig[0] / timesig[1])
+    measure = 1 + int(beat / beats_per_measure)
+
+    return measure
+
+
+class Config:
     """
+    @DynamicAttrs
+    This class provides a cleaner API than just using the args dictionary directly.
+    It transforms the CLI parameters into class attributes (leading dashes are removed and
+    dashes inside the name are replaced with an underscore).
+    It also sets up some functions to perform actions which depend on the configuration.
+    """
+
     def __init__(self, args):
-        self.__dict__ = {k.replace("--", "").replace("<", "").replace(">", "").replace("-", "_") : v for k, v in args.items()}
+        # hack to automatically convert command line args to config attributes
+        self.__dict__ = {k.replace("--", "").replace("<", "").replace(">", "").replace("-", "_"): v for k, v in
+                         args.items()}
+
+        # extra attributes that don't come from the command line
+        self.tempo = None
+        self.key = None
+        self.timesig = None
+        self.ppqn = None
+
+        # functions
         self.note2str = str if self.no_note_trans else m2h_note
-        self.time2str = str if self.no_time_trans else lambda t: str(mido.tick2second(t, self.ppqn, self.tempo)) + "s"
+        self.time2str = str if self.no_time_trans else lambda t: str(
+            midi_time_to_seconds(t, self.ppqn, self.tempo)) + "s"
+
 
 class Action:
     """ A class which represents an action performed on the piano """
+
     def __init__(self, press, note, time, config):
         self.press = press
         self.note = note
@@ -88,54 +120,125 @@ class Action:
         self.config = config
 
     def __str__(self):
-        return f"{'Press' if self.press else 'Release'} note {self.config.note2str(self.note)} at {self.config.time2str(self.time)}"
+        action_str = "Press" if self.press else "Release"
+        note_str = self.config.note2str(self.note)
+        time_str = self.config.time2str(self.time)
+        measure = midi_time_to_measure(self.time, self.config.ppqn, self.config.timesig)
+        return f"{action_str} note {note_str} at measure {measure} ({time_str})"
 
 
-def verify_restrictions(pressed_notes, config):
-    sorted_pressed_notes = sorted(pressed_notes)
-    
-    # piano restrictions
-    if not config.no_piano_res:
-        # note range check
-        for n in pressed_notes:
-            if not config.lowest_key <= n <= config.highest_key:
-                return f"Note out of range. Must be between {config.note2str(config.lowest_key)} and {config.note2str(config.highest_key)}."
-    
-    # player restrictions
-    if not config.no_player_res:
-        # max notes check
-        if len(pressed_notes) > config.max_notes:
-            return f"Too many notes held at once ({len(pressed_notes)}), maximum is {config.max_notes}.\n" \
-                   f"Notes pressed at once: {', '.join(map(config.note2str, sorted_pressed_notes))}."
+class MidiChecker:
+    def __init__(self, config):
+        # opens midi file and sets up some variables
+        self.midi = mido.MidiFile(config.file)
+        self.config = config
+        self.config.ppqn = self.midi.ticks_per_beat
 
-        # hands & fingers check (assumes min 1 hand and 1 finger per hand)
-        hand_keys = [[sorted_pressed_notes[0]]]
-        hand = 0
-        for n in sorted_pressed_notes[1:]:
-            if len(hand_keys[hand]) == config.fingers or key_distance(hand_keys[hand][0], n) > config.span:
-                hand += 1
-                if hand == config.hands:
-                    hand_keys_str = '\n'.join(f"Hand {h}: {', '.join(map(config.note2str, hand_keys[h]))}." for h in range(config.hands))
-                    return f"Not enough hands/fingers to press all the keys. Keys pressed by each hand:\n" \
-                           f"{hand_keys_str}\n" \
-                           f"Then trying to also hold {config.note2str(n)}."
-                hand_keys.append([n])
+    def parse_events(self):
+        # creates a list of actions (press/release note) with their associated timestamp
+        actions = []
+        for i, track in enumerate(self.midi.tracks):
+            acc_time = 0
+            for msg in track:
+                # assume MIDI song metadata is at some event at time 0
+                # TODO: for now, assume tempo and time sig don't change for the whole song
+                if msg.time == 0:
+                    if msg.type == "set_tempo":
+                        self.config.tempo = msg.tempo
+                    elif msg.type == "key_signature":
+                        self.config.key = msg.key
+                    elif msg.type == "time_signature":
+                        self.config.timesig = (msg.numerator, msg.denominator)
+
+                acc_time += msg.time
+                if msg.type in ["note_on", "note_off"]:
+                    actions.append(Action(msg.type == "note_on", msg.note, acc_time, self.config))
+
+        # sort actions by acc time (needed if the midi has more than one track)
+        actions.sort(key=lambda a: a.time)
+
+        return actions
+
+    def verify_actions(self, actions):
+        # analizes all note intersections to determine if they are playable with the given restrictions
+        no_fails = True
+        pressed_notes = []
+        for a in actions:
+            if a.press:
+                if a.note in pressed_notes:
+                    if not self.config.hide_warnings:
+                        print(f"Warning: Note press while note is already being pressed, in action '{a}'")
+                else:
+                    pressed_notes.append(a.note)
+                violation = self._verify_restrictions(pressed_notes)
+                if violation is not None:
+                    print(f"Fail: Restrictions not satisfied, in action '{a}'")
+                    indent = "      "
+                    print(indent + violation.replace("\n", "\n" + indent))
+                    no_fails = False
+                    if self.config.abort_on_fail:
+                        exit(1)
             else:
-                hand_keys[hand].append(n)
+                if a.note not in pressed_notes:
+                    if not self.config.hide_warnings:
+                        print(f"Warning: Note release while note is not being pressed, in action '{a}'")
+                else:
+                    pressed_notes.remove(a.note)
 
-    return None
+        if no_fails:
+            print("All tests passed!")
 
-if __name__ == "__main__":
+    def _verify_restrictions(self, pressed_notes):
+        sorted_pressed_notes = sorted(pressed_notes)
+
+        # piano restrictions
+        if not self.config.no_piano_res:
+            # note range check
+            for n in pressed_notes:
+                if not self.config.lowest_key <= n <= self.config.highest_key:
+                    return f"Note out of range. Must be between {self.config.note2str(self.config.lowest_key)} and {self.config.note2str(self.config.highest_key)}."
+
+        # player restrictions
+        if not self.config.no_player_res:
+            # max notes check
+            if len(pressed_notes) > self.config.max_notes:
+                return f"Too many notes held at once ({len(pressed_notes)}), maximum is {self.config.max_notes}.\n" \
+                       f"Notes pressed at once: {', '.join(map(self.config.note2str, sorted_pressed_notes))}."
+
+            # hands & fingers check (assumes min 1 hand and 1 finger per hand)
+            hand_keys = [[sorted_pressed_notes[0]]]
+            hand = 0
+            for n in sorted_pressed_notes[1:]:
+                if len(hand_keys[hand]) == self.config.fingers or key_distance(hand_keys[hand][0],
+                                                                               n) > self.config.span:
+                    hand += 1
+                    if hand == self.config.hands:
+                        hand_keys_str = '\n'.join(
+                            f"Hand {h}: {', '.join(map(self.config.note2str, hand_keys[h]))}." for h in
+                            range(self.config.hands))
+                        return f"Not enough hands/fingers to press all the keys. Keys pressed by each hand:\n" \
+                               f"{hand_keys_str}\n" \
+                               f"Then trying to also hold {self.config.note2str(n)}."
+                    hand_keys.append([n])
+                else:
+                    hand_keys[hand].append(n)
+
+        return None
+
+
+def main():
     # automatic arg parsing from module docstring
     args = docopt(__doc__)
     if args["<file>"] is None:
         print(__doc__)
         exit(0)
+
     # handles --max-notes special default case
     def_max_notes = False
     if args["--max-notes"] == "hands*fingers":
         del args["--max-notes"]
         def_max_notes = True
+
     # must parse args before calculating default value of --max-notes
     parse_args(args, arg_parsers)
     if def_max_notes:
@@ -143,58 +246,15 @@ if __name__ == "__main__":
     # instantiantes a config object with the provided arguments
     config = Config(args)
 
-    # opens midi file and sets up some variables
-    midi = mido.MidiFile(config.file)
-    config.ppqn = midi.ticks_per_beat
-    config.tempo = 500000 # TODO: extract this info from MIDI file
+    midi_checker = MidiChecker(config)
+    actions = midi_checker.parse_events()
+    midi_checker.verify_actions(actions)
 
-    # creates a list of actions (press/release note) with their associated timestamp
-    actions = []
-    for i, track in enumerate(midi.tracks):
-        acc_time = 0
-        for msg in track:
-            # experimental
-            # if msg.type == "set_tempo":
-            #     config["tempo"] = msg.tempo
-            # if msg.type == "key_signature":
-            #     config["key"] = msg.key
-            # if msg.type == "time_signature":
-            #     config["timesig"] = (msg.numerator, msg.denominator)
-            acc_time += msg.time
-            if msg.type in ["note_on", "note_off"]:
-                actions.append(Action(msg.type == "note_on", msg.note, acc_time, config))
 
-    # analizes all note intersections to determine if they are playable with the given restrictions
-    no_fails = True
-    pressed_notes = []
-    for a in actions:
-        if a.press:
-            if a.note in pressed_notes:
-                if not config.hide_warnings:
-                    print(f"Warning: Note press while note is already being pressed, in action '{a}'")
-            else:
-                pressed_notes.append(a.note)
-            violation = verify_restrictions(pressed_notes, config)
-            if violation is not None:
-                print(f"Fail: Restrictions not satisfied, in action '{a}'")
-                indent = "      "
-                print(indent + violation.replace("\n", "\n" + indent))
-                no_fails = False
-                if config.abort_on_fail:
-                    exit(1)
-        else:
-            if a.note not in pressed_notes:
-                if not config.hide_warnings:
-                    print(f"Warning: Note release while note is not being pressed, in action '{a}'")
-            else:
-                pressed_notes.remove(a.note)
-    
-    if no_fails:
-        print("All tests passed!")
+if __name__ == "__main__":
+    main()
 
 """ TODO: Future features
-- Support multi-track MIDI files
-- Be measure-aware (i.e. instead of giving the time of the action in seconds, give the measure and position inside the measure)
 - Allow to have a different amount of fingers on each hand
 - Add restrictions related to the speed, finger patterns, etc.
 - Take pedals into account (e.g. you can use sustain instead of keeping a note pressed to have more free fingers)
